3825173e9313e09ab0d37a4dd8160d3462c3b29d
OwenSun9812 <sun.jingk@northeastern.edu>
2023-01-16 18:02:58 -0500

    Lab0

---
+++ Lab0/src/pubandsub/scripts/listener.py
@@ -0,0 +1,20 @@
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + 'Yes, I heard you on computer%s', data.data)
+
+def listener():
+
+  
+    rospy.init_node('listener', anonymous=True)
+    
+  
+
+    rospy.Subscriber('chatter', String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
