833196b7374d1550aae5583c09819f6ed8ca576a
OwenSun9812 <122328519+OwenSun9812@users.noreply.github.com>
2023-02-08 16:08:37 -0500

    Add files via upload

---
+++ LAB1/src/Analysis/Analysis scripts for altitude and time.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import bagpy
+from bagpy import bagreader
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sea
+import pandas as pd
+
+bag = bagreader('/home/owensun/catkin_ws/src/gps_driver0/data/walk.bag')
+bag.topic_table
+data = bag.message_by_topic('/GPSDATA')
+readings = pd.read_csv(data)
+readings['altitude'] = readings['altitude'] - readings['altitude'].min()
+plt.scatter(range(len(readings['altitude'])), readings['altitude'])
+plt.xlabel('time(s)')
+plt.ylabel('Altitude(m)')
+plt.title('Station_occluded')
+plt.show()
